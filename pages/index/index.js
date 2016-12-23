34aeb1caf4d9c3aca1e3b711de969fa4ba468bc0
ivyxuan <iloveivyxuan@gmail.com>
2016-12-23 17:39:26 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,38 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    systemInfo: {},
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  invitationClick: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function (options) {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取系统数据
+    app.getSystemInfo(function(res) {
+      that.setData({
+        systemInfo: res
+      })
+    })
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  }
+})
